d769fdcc335191e9b05c8504bc9e4549e471fdb3
Mohamed Rafeek <mohamed.rafeek@mohamed.rafeek-MacBook-Pro>
2024-11-12 15:00:54 -0500

    initial commit

--- index.test.js
+++ index.test.js
@@ -12,11 +12,42 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
 
+    it("endpoint is functional", async () =>{
+        const response = await request(app).get('/musicians')
+        expect(response.statusCode).toBe(200)
+    })
 
+    it("endpoint returns correct info", async () =>{
+        const response = await request(app).get('/musicians')
 
+        const responseData = JSON.parse(response.text)
 
+        //make sure array is not empty
+        expect(responseData.length).toBeGreaterThan(0);
+        
+        //check that each property has necessary fields:
+
+        responseData.forEach(item => {
+            expect(item).toHaveProperty("id")
+            expect(item).toHaveProperty("name")
+            expect(item).toHaveProperty("instrument")
+            expect(item).toHaveProperty("createdAt")
+            expect(item).toHaveProperty("updatedAt")
+        });
+        
+        //first name in array should be mick jagger
+        expect(responseData[0].name).toBe("Mick Jagger")
+
+    })
     
 })
+
+describe('./bands endpoint', () => {
+
+    test("band endpoint is functional", async () => {
+        const response = await request(app).get('/bands')
+        expect(response.statusCode).toBe(200)
+    })
+})
+
